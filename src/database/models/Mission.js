823e0a67a2e781120c5f5aed1ff0c844de4d0230
Mohan <102119043@nitt.edu>
2020-12-23 19:21:57 -0400

    Feat : Schema for mission and hints 👍

---
+++ src/database/models/Mission.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const MissionSchema = new Schema({
+  Category: {
+    type: String,
+    required: true,
+  },
+  clue: {
+    type: String,
+    required: true,
+  },
+  answer_Type: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  Location: {
+    Lat: {
+      type: String,
+    },
+    Long: {
+      type: String,
+    },
+  },
+  Other_Info: {
+    type: String,
+  },
+  Hints: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Hint",
+    },
+  ],
+});
+module.exports = model("Mission", MissionSchema);
