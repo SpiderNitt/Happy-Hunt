ea8d1fdda87b495d2e8aa631dabb5aae3dff29a7
paarthibank <63107117+paarthibank@users.noreply.github.com>
2020-12-24 00:08:23 +0530

    Created activity schema

---
+++ src/database/models/Activity.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const activitySchema = new mongoose.Schema({
+  team: {
+    type: mongoose.Types.ObjectId,
+    ref: "Team",
+  },
+  content: String,
+  status: Boolean,
+  count: Number,
+  photo: String,
+});
