969451d42d29478112faffc5b7365bda71bde8e0
highonweb <mothish07122001@outlook.com>
2020-12-25 00:52:57 +0530

    Feat:invite links sent to the emails of hunters 🦊

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,9 @@
 const auth = require("express").Router();
 const NewAdmin = require("./routers/Create_admin");
 const AdminRouter = require("./routers/Admin_auth");
-const team = require("./routers/team");
+const team = require("./routers/teamRouter");
 
 auth.use("/login/admin", AdminRouter);
 auth.use("/create_admin", NewAdmin);
-auth.use("/team");
+auth.use("/team", team);
 module.exports = auth;
--- src/api/routers/teamRouter.js
+++ src/api/routers/teamRouter.js
@@ -1,20 +1,33 @@
 const team = require("express").Router();
 const { uid } = require("uid");
+const nodemailer = require("nodemailer");
 const Team = require("../../database/models/Team");
 const User = require("../../database/models/User");
 const { jwtVerify } = require("../../middlewares/jwt");
 
 team.post("/create", jwtVerify, async (req, res) => {
   try {
-    const { teamName } = req.body;
-
+    const { teamName, emails } = req.body;
     if (teamName == null) {
       return res.status(200).json({ Message: "Fill all the fields " });
     }
+    const testAccount = await nodemailer.createTestAccount();
 
-    const user = await User.findOne({ id: req.jwt_payload.id });
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+
+    const user = await User.findOne({ Id: req.jwt_payload.id });
+    console.log(user);
     let teamId = uid();
-    while (await Team.exists({ teamId: teamId })) {
+    while (await Team.exists({ teamId })) {
       teamId = uid();
     }
 
@@ -24,11 +37,20 @@ team.post("/create", jwtVerify, async (req, res) => {
       members: [],
     });
     newTeam.members.push(user);
-    newTeam.save();
+    await newTeam.save();
 
-    return res
-      .status(200)
-      .json({ Message: "Team created Successfully. Happy Hunting!!" });
+    const info = await transporter.sendMail({
+      from: `"${user.name}" <info@happyhunt.com>`,
+      to: emails.join(", "),
+      subject: "Invite to the greatest hunt ever",
+      html: `<a href='localhost:8000/auth/team/join?teamid=${newTeam.teamId}' >click the link to join my team</a>`,
+    });
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    return res.status(200).json({
+      Message: "Team created Successfully. Happy Hunting!!",
+      Team: newTeam,
+    });
   } catch (error) {
     console.log(error);
     res
@@ -38,24 +60,27 @@ team.post("/create", jwtVerify, async (req, res) => {
   return 0;
 });
 
-team.create("/join", async (req, res) => {
+team.get("/join", jwtVerify, async (req, res) => {
   try {
-    const { teamId } = req.body;
-    if (teamId == null) {
+    const { teamid } = req.query;
+    if (teamid == null) {
       return res.status(200).json({ Message: "Fill all the fields " });
     }
 
-    const user = await User.findOne({ id: req.jwt_payload.id });
-    const existingTeam = await Team.findOne({ teamId });
+    const user = await User.findOne({ Id: req.jwt_payload.id });
+    const existingTeam = await Team.findOne({ teamId: teamid });
     existingTeam.members.push(user);
 
     return res
       .status(200)
-      .json({ Message: "Team joined Successfully. Happy Hunting!!" });
+      .json({ Message: "joined to the Team Successfully. Happy Hunting!!" });
   } catch (error) {
+    console.log(error);
     res
       .status(500)
       .json({ Message: "Internal Server Error, Try again later!!" });
   }
   return 0;
 });
+
+module.exports = team;
