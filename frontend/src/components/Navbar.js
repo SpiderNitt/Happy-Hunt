bdebef7bd97f180f9ef226e65a63456110bdb65d
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2020-12-26 16:26:12 +0530

    add leaderbaord and activity feed front (#12)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,13 +11,16 @@ import {
   import MenuIcon from "@material-ui/icons/Menu";
   import React, { useState, useEffect } from "react";
   import { Link as RouterLink } from "react-router-dom";
-  
+  import HomeIcon from '@material-ui/icons/Home';
+  import Typography from '@material-ui/core/Typography';
+
   const navData = [
     {
-      label: "Home",
+      label: <HomeIcon/>,
       href: "/",
     }
   ];
+
   
   const useStyles = makeStyles(() => ({
     header: {
@@ -32,26 +35,56 @@ import {
       marginLeft: "38px",
     },
     toolbar: {
-      justifyContent: "space-between",
+      display:"flex",
+      justifyContent:"space-between"
     },
     menuBox: {
       padding: "10px 10px",
     },
+    timer: {
+      color:"white",
+    },
   }));
   
-  function Navbar() {
+  function Navbar(props) {
     const { header,  menuButton, toolbar, menuBox } = useStyles();
-  
+    const classes = useStyles();
     const [state, setState] = useState({
       mobileView: false,
       menuopen: false,
     });
-  
+
+    const [time, setTime] = useState({
+      h:0, m:0 , sec:0
+    })
+
+    var updatedSec= time.sec, updatedM= time.m, updatedH= time.h;
+
+    const runTimer=()=>{
+      if(updatedM==60){
+        updatedH++;
+        updatedM=0
+      }if(updatedSec==60){
+        updatedM++;
+        updatedSec=0
+      }
+      updatedSec++
+
+      return setTime({
+        h:updatedH, m:updatedM , sec:updatedSec
+      })
+    }
+
+    const run=()=>{
+      runTimer();
+      setInterval(runTimer, 1000)
+    }
     const { mobileView, menuopen } = state;
   
     useEffect(() => {
+      
       const setResponsiveness = () => {
-        return window.innerWidth < 900
+        return window.innerWidth < 400
           ? setState((prevState) => ({ ...prevState, mobileView: true }))
           : setState((prevState) => ({ ...prevState, mobileView: false }));
       };
@@ -63,10 +96,18 @@ import {
   
     const displayDesktop = () => {
       return (
-        <Toolbar className={toolbar}>
-          Happy hunt
-          <div>{getMenuButtons()}</div>
-        </Toolbar>
+        <div>
+          <Toolbar className={classes.toolbar} >
+            <h2> Happy hunt </h2>
+            <Typography variant="span" className={classes.timer}>
+              {(time.h)>= 10? time.h:"0" + time.h} : {(time.m)>= 10? time.m:"0" + time.m} : {(time.sec)>= 10? time.sec:"0" + time.sec}
+            </Typography>
+            <div>{getMenuButtons()}</div>
+          </Toolbar>
+          <Button variant="contained" color="primary"  onClick={run} style={{margin:5}} >
+           start hunt !
+          </Button>
+        </div>
       );
     };
   
@@ -89,7 +130,9 @@ import {
           >
            <MenuIcon />
           </IconButton>
-
+            <Typography variant="span" className={classes.timer}>
+              {(time.h)>= 10? time.h:"0" + time.h} : {(time.m)>= 10? time.m:"0" + time.m} : {(time.sec)>= 10? time.sec:"0" + time.sec}
+            </Typography>
           <Menu
              {...{
                 open: menuopen,
