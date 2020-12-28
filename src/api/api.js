bc75f755e88dd6d54281ca0fa814caafca903ab8
Mohan <102119043@nitt.edu>
2020-12-28 11:36:05 -0400

    Feat: route for updation of status 👑👑

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 const api = require("express").Router();
 
 const scoreboard = require("./routers/scoreboard");
+const admin = require("./routers/Admin_route");
 
 api.use("/scoreboard", scoreboard);
+api.use("/admin", admin);
 module.exports = api;
