d33d5eb91309f14c8504317be921aac91ec9aea9
unknown <mansibisen354@gmail.com>
2020-12-28 18:50:12 +0530

    route: Activity feed for user and admin with feat of liking

---
+++ src/api/routers/activity.js
@@ -0,0 +1,32 @@
+const Router = require("express").Router();
+const Feed = require("../../database/models/Activity");
+
+Router.get("/feed", async (req, res) => {
+  try {
+    const feeds = await Feed.find({ status: "accepted", ShouldBeShown: true });
+    res.status(200).json({ actiityFeeds: feeds });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server Error ",
+    });
+  }
+});
+Router.get("/feed/likes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    let feed = await Feed.findById(id);
+    if (!feed) {
+      res.status(404).json({ message: "no such feed found" });
+    }
+    feed.likes += 1;
+    feed.save();
+    res.status(200).json({ message: "post liked" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server Error ",
+    });
+  }
+});
+module.exports = Router;
