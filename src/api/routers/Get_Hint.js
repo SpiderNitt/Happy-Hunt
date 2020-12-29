77e3faa8aea4bdc75a5031c959456ece3d5c185f
unknown <mansibisen354@gmail.com>
2020-12-30 00:42:32 +0530

    route: getting hinst to the user and updating in database

--- src/api/routers/Get_Hint.js
+++ src/api/routers/Get_Hint.js
@@ -1,10 +1,31 @@
 const Router = require("express").Router();
-const Team = require("../../database/models/Team");
+
 const Mission = require("../../database/models/Mission");
+const Activity = require("../../database/models/Activity");
 
 Router.get("/", async (req, res) => {
-  let { Mission_Id } = req.body;
-  let mission = await Mission.findById(Mission_Id);
+  try {
+    const { MissionId } = req.body;
+    const mission = await Mission.findById(MissionId);
+    const hint = mission.Hints;
+    const activity = await Activity.findOne({
+      team: req.jwt_payload.team,
+      mission: MissionId,
+    });
+    const HintNumber = activity.hintsTaken;
+    if (HintNumber < 3) {
+      activity.hintsTaken += 1;
+      activity.save();
+
+      return res.status(200).json({ hint: hint[HintNumber] });
+    }
+    return res.status(403).json({ message: "No more hints available" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server Error ",
+    });
+  }
 });
 
 module.exports = Router;
