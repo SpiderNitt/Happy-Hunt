6c6e50935f086e174ce0de330d46d9d55d65fbf9
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2020-12-29 13:50:03 +0530

    ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
 import "./App.css";
-import Navbar from './components/Navbar';
-import Starting from './components/Starting';
-import Welcome from './components/Welcome';
-import Rules from './components/Rules';
-import MainClues from './components/MainClues';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Activity from "./components/Activity";
-import Leaderboard from "./components/Leaderbaord";
-import Notifications from "./components/Notifications";
+import UserNav from "./navigation/userNavigation";
 
 
 function App() {
 
   return (
-    <Router>
-      <div className='App'>
-      <Navbar/>
-        <Switch>
-          <Route path="/" exact component={Welcome}/>
-          <Route path="/starting" component={Starting}/>
-          <Route path="/rules" component={Rules}/>
-          <Route path="/main" component={MainClues}/>
-          <Route path="/activity" exact component={Activity}/>
-          <Route path="/leaderboard" component={Leaderboard}/>
-          <Route path="/notifications" component={Notifications}/>
-          
-        </Switch>
-        </div>
-    </Router>
+    <div className='App'>
+        <UserNav/>
+    </div>
     
   )
 
