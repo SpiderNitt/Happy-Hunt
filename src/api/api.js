7c41bba6ad589fd4dacb0a9f40f82954e98014eb
unknown <mansibisen354@gmail.com>
2020-12-30 09:46:16 +0530

    api route integrated with hints

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
 const api = require("express").Router();
 
 const scoreboard = require("./routers/scoreboard");
+const hintRoute = require("./routers/Get_Hint");
 
 api.use("/scoreboard", scoreboard);
+api.use("/hint", hintRoute);
 module.exports = api;
