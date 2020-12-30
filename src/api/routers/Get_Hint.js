d9de5243fd817c9c7c26d11e51b314028740c82e
unknown <mansibisen354@gmail.com>
2020-12-30 14:00:32 +0530

    hints added

--- src/api/routers/Get_Hint.js
+++ src/api/routers/Get_Hint.js
@@ -2,22 +2,22 @@ const Router = require("express").Router();
 
 const Mission = require("../../database/models/Mission");
 const Activity = require("../../database/models/Activity");
-
+const Hint = require("../../database/models/Hint")
 Router.get("/", async (req, res) => {
   try {
     const { MissionId } = req.body;
     const mission = await Mission.findById(MissionId);
     const hint = mission.Hints;
     const activity = await Activity.findOne({
-      team: req.jwt_payload.team,
+      team: "5fe641ef1da3c71e2c3c0222",
       mission: MissionId,
     });
     const HintNumber = activity.hintsTaken;
     if (HintNumber < 3) {
       activity.hintsTaken += 1;
       activity.save();
-
-      return res.status(200).json({ hint: hint[HintNumber] });
+      let hintStatement = await Hint.findById(hint[HintNumber] )
+      return res.status(200).json({ hint: hintStatement });
     }
     return res.status(403).json({ message: "No more hints available" });
   } catch (err) {
