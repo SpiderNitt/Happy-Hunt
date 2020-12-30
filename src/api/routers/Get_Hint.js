8510b7952aa2ed7465c3ef050ef867ca21215319
unknown <mansibisen354@gmail.com>
2020-12-30 16:02:04 +0530

    final feature in hint added

--- src/api/routers/Get_Hint.js
+++ src/api/routers/Get_Hint.js
@@ -1,22 +1,23 @@
 const Router = require("express").Router();
-
+const { jwtVerify } = require("../../middlewares/jwt");
 const Mission = require("../../database/models/Mission");
 const Activity = require("../../database/models/Activity");
-const Hint = require("../../database/models/Hint")
-Router.get("/", async (req, res) => {
+const Hint = require("../../database/models/Hint");
+
+Router.get("/", jwtVerify, async (req, res) => {
   try {
     const { MissionId } = req.body;
     const mission = await Mission.findById(MissionId);
     const hint = mission.Hints;
     const activity = await Activity.findOne({
-      team: "5fe641ef1da3c71e2c3c0222",
+      team: req.jwt_payload.team,
       mission: MissionId,
     });
     const HintNumber = activity.hintsTaken;
     if (HintNumber < 3) {
       activity.hintsTaken += 1;
       activity.save();
-      let hintStatement = await Hint.findById(hint[HintNumber] )
+      const hintStatement = await Hint.findById(hint[HintNumber]);
       return res.status(200).json({ hint: hintStatement });
     }
     return res.status(403).json({ message: "No more hints available" });
