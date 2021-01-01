add708ba3f35e6af362eb9d9491f126f59fd3c74
unknown <mansibisen354@gmail.com>
2021-01-01 20:34:38 +0530

    second layer of eslint errors fixed

--- src/api/routers/activity.js
+++ src/api/routers/activity.js
@@ -5,7 +5,7 @@ const { jwtVerify } = require("../../middlewares/jwt");
 Router.get("/feed", jwtVerify, async (req, res) => {
   try {
     const feeds = await Feed.find({ status: "accepted", ShouldBeShown: true });
-    res.status(200).json({ actiityFeeds: feeds });
+    return res.status(200).json({ actiityFeeds: feeds });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
@@ -18,13 +18,13 @@ Router.get("/feed/likes/:id", jwtVerify, async (req, res) => {
     const { id } = req.params;
     const feed = await Feed.findById(id);
     if (!feed) {
-      res.status(404).json({ message: "no such feed found" });
+      return res.status(404).json({ message: "no such feed found" });
     }
     console.log("previous", feed.likes);
     feed.likes += 1;
     console.log("after", feed.likes);
     feed.save();
-    res.status(200).json({ message: "post liked" });
+    return res.status(200).json({ message: "post liked" });
   } catch (err) {
     console.log(err);
     return res.status(500).json({
