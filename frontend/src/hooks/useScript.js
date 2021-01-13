b6322d3c00df574f21adf11a18e941ea34e5e571
gokul-00 <106119141@nitt.edu>
2021-01-13 22:02:35 +0530

    payment added

---
+++ frontend/src/hooks/useScript.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const useScript = (url, classElement, attribute) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = url;
+    script.async = true;
+    script.setAttribute(attribute.key, attribute.value);
+    document.getElementsByClassName(classElement)[0].appendChild(script);
+
+    return () => {
+      document.getElementsByClassName(classElement)[0].removeChild(script);
+    };
+  }, [attribute.key, attribute.value, classElement, url]);
+};
+
+export default useScript;
