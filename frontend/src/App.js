d5111ac033bff557be4d0105d59076eb37a0864d
Sai <saisandhya187@gmail.com>
2021-01-14 10:03:50 +0530

    Updated App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import UserNav from "./navigation/userNavigation";
+import AdminNav from "./navigation/AdminNavigation";
+import AuthContext from "./api/authContext";
+
+function App() {
+  const [user, setUser] = useState();
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <div className='App'>
+        <AdminNav />
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
