3eb6147aa7300133fe18fcff33c14059e5714ed7
Sai <saisandhya187@gmail.com>
2021-01-14 18:26:17 +0530

    Added integ for admin mission list

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,9 +2,6 @@ import apisauce from "apisauce";
 
 const client = apisauce.create({
   baseURL: "http://localhost:5000/",
-  headers: {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZmQ5ZGU5Mjg2MDVkMjUyNDEwZmM4OSIsImlhdCI6MTYxMDUyMTg2MSwiZXhwIjoxNjExMTI2NjYxfQ.yHKm_ktg5H_Qv1m2d7w4Dvqh5Qewyibju4p4qrrQ-xc"
-  }
 });
 
 client.addAsyncRequestTransform(async (request) => {
