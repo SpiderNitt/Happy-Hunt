a768bcc4b0939b5d8e63c128260f5185dc91463c
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-16 23:56:01 +0530

    add user clues integ

---
+++ frontend/src/user/ClueItems.jsx
@@ -0,0 +1,55 @@
+import {React, useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Chip, Container, Grid } from '@material-ui/core';
+
+const RenderClues=(props)=> {
+   const classes= useStyles();
+   
+    return (
+      <Card key={props.key}  style={{ marginBottom: 10, padding: 10 }}>
+        <CardContent>
+          <div className={classes.container}>
+            <Typography className={classes.title} color="textSecondary" variant="ul" >
+              Clue : {props.mission.cluename}
+            </Typography>
+            
+            <Chip size="small" label={props.mission.isSolved? "solved" : "unsolved" } />
+          </div>
+        </CardContent>
+        <CardActions style={{ display: 'flex', justifyContent: 'space-between'}}>
+          <Button variant="contained" size="small" href="/photo-clue">View</Button>
+          <Typography color="textSecondary">
+            {props.mission.points}
+          </Typography>
+        </CardActions>
+      </Card>
+    );
+}
+
+export default RenderClues;
+
+const useStyles = makeStyles({
+    title: {
+      fontSize: 20,
+      color:"olive",
+      fontWeight: 'bold',
+      textAlign: 'left',
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    points: {
+      float:"right",
+    },
+    container: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 10,
+    }
+  });
