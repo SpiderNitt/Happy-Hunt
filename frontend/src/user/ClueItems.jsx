ce43b61f5dc6335c46492b46b6ecfbe872ad9292
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-19 00:07:00 +0530

    clues page fix

--- frontend/src/user/ClueItems.jsx
+++ frontend/src/user/ClueItems.jsx
@@ -23,9 +23,9 @@ const RenderClues=({mission, index})=> {
         </CardContent>
         <CardActions style={{ display: 'flex', justifyContent: 'space-between'}}>
           <Button variant="contained" size="small" href="/photo-clue">View</Button>
-          <Typography color="textSecondary">
+          {/* <Typography color="textSecondary">
             {mission.maxPoints}
-          </Typography>
+          </Typography> */}
         </CardActions>
       </Card>
       
