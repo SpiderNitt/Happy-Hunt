d0a4f7afda36b656f52146ae6ffb308e5ebc8b90
paarthibank <63107117+paarthibank@users.noreply.github.com>
2021-01-19 23:32:10 +0530

    Added adminlist route made changes to mission route added role verification middleware

--- src/api/routers/openApi.js
+++ src/api/routers/openApi.js
@@ -2,6 +2,7 @@ const openApi = require("express").Router();
 const Team = require("../../database/models/Team");
 const Mission = require("../../database/models/Mission");
 const User = require("../../database/models/User");
+const Hint = require("../../database/models/Hint");
 
 openApi.get("/scoreboard", async (req, res) => {
   try {
@@ -22,7 +23,15 @@ openApi.get("/mission/:id", async (req, res) => {
     if (!mission) {
       return res.status(404).json({ message: "Mission not found" });
     }
-    return res.status(200).json(mission);
+    const hint = [];
+    let i;
+    for (i = 0; i < mission.Hints.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const result = await Hint.findById(mission.Hints[i]);
+      hint.push(result);
+    }
+    const result = { mission, hint };
+    return res.status(200).json(result);
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({ Message: "Server Error, Try again later" });
@@ -54,6 +63,17 @@ openApi.post("/payment", async (req, res) => {
       return res.status(200).json({ message: "Success" });
     }
     return res.status(400).json({ message: "Unable to update record" });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ message: "Server Error, Try again later" });
+  }
+});
+openApi.get("/adminList", async (req, res) => {
+  try {
+    const adminList = await User.find({ Role: "Admin" });
+    if (!adminList)
+      return res.status(404).json({ message: "Admin list is empty" });
+    return res.status(200).json(adminList);
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({ message: "Server Error, Try again later" });
---
+++ src/middlewares/role.js
@@ -0,0 +1,20 @@
+const adminVerify = async (req, res, next) => {
+  if (req.jwt_payload.role !== "Admin")
+    return res.status(400).json({ message: "Access denied" });
+  return next();
+};
+const playerVerify = async (req, res, next) => {
+  if (
+    req.jwt_payload.role !== "Player" ||
+    req.jwt_payload.role !== "TeamMember" ||
+    req.jwt_payload.role !== "TeamLeader"
+  )
+    return res.status(400).json({ message: "Access denied" });
+  return next();
+};
+const superAdminVerify = async (req, res, next) => {
+  if (req.jwt_payload.role !== "SuperAdmin")
+    return res.status(400).json({ message: "Access denied" });
+  return next();
+};
+module.exports = { adminVerify, playerVerify, superAdminVerify };
