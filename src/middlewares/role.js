6546b0476f0d5836c4e9908443df798867d01ff2
paarthibank <63107117+paarthibank@users.noreply.github.com>
2021-01-20 13:42:56 +0530

    Made changes to role middleware

--- src/middlewares/role.js
+++ src/middlewares/role.js
@@ -1,12 +1,12 @@
 const adminVerify = async (req, res, next) => {
-  if (req.jwt_payload.role !== "Admin")
+  if (req.jwt_payload.role !== "Admin" && req.jwt_payload.role !== "SuperAdmin")
     return res.status(400).json({ message: "Access denied" });
   return next();
 };
 const playerVerify = async (req, res, next) => {
   if (
-    req.jwt_payload.role !== "Player" ||
-    req.jwt_payload.role !== "TeamMember" ||
+    req.jwt_payload.role !== "Player" &&
+    req.jwt_payload.role !== "TeamMember" &&
     req.jwt_payload.role !== "TeamLeader"
   )
     return res.status(400).json({ message: "Access denied" });
