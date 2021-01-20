b26c3b4a08566a55d0d9046078ffc2dcb0b296d9
unknown <mansibisen354@gmail.com>
2021-01-20 18:54:49 +0530

    role typo fixed in middleware

--- src/middlewares/role.js
+++ src/middlewares/role.js
@@ -1,19 +1,19 @@
 const adminVerify = async (req, res, next) => {
-  if (req.jwt_payload.role !== "Admin" && req.jwt_payload.role !== "SuperAdmin")
+  if (req.jwt_payload.Role !== "Admin" && req.jwt_payload.Role !== "SuperAdmin")
     return res.status(400).json({ message: "Access denied" });
   return next();
 };
 const playerVerify = async (req, res, next) => {
   if (
-    req.jwt_payload.role !== "Player" &&
-    req.jwt_payload.role !== "TeamMember" &&
-    req.jwt_payload.role !== "TeamLeader"
+    req.jwt_payload.Role !== "Player" &&
+    req.jwt_payload.Role !== "TeamMember" &&
+    req.jwt_payload.Role !== "TeamLeader"
   )
     return res.status(400).json({ message: "Access denied" });
   return next();
 };
 const superAdminVerify = async (req, res, next) => {
-  if (req.jwt_payload.role !== "SuperAdmin")
+  if (req.jwt_payload.Role !== "SuperAdmin")
     return res.status(400).json({ message: "Access denied" });
   return next();
 };
