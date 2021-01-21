300396d3b1cf50689ff8443e6a9e9136eca1fb15
unknown <mansibisen354@gmail.com>
2021-01-21 21:52:03 +0530

    delete admin changed from req.body to params

--- src/api/routers/AdminRoute.js
+++ src/api/routers/AdminRoute.js
@@ -66,42 +66,30 @@ Router.post(
     }
   }
 );
-Router.delete(
-  "/deleteAdmin",
-  AdminCreateValidator,
-  superAdminVerify,
-  async (req, res) => {
-    try {
-      const issuperadmin = await User.findById(req.jwt_payload.id);
-      if (issuperadmin.Role !== "SuperAdmin") {
-        return res.status(402).json({
-          message: "You don't have permission to perform the operation",
-        });
-      }
-      const { emailId } = req.body;
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      if (emailId !== undefined) {
-        if (!(await User.findOne({ emailId }))) {
-          return res.status(404).json({ message: "no such user found" });
-        }
+Router.delete("/deleteAdmin", superAdminVerify, async (req, res) => {
+  try {
+    const { emailId } = req.query;
 
-        await User.deleteOne({ emailId });
-        return res.status(200).json({ message: "user deleted sucessfully" });
-      }
-      return res
-        .status(401)
-        .json({ message: "emailId provided was undefined" });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({
-        message: "Server Error ",
-      });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    if (emailId !== undefined) {
+      if (!(await User.findOne({ emailId }))) {
+        return res.status(404).json({ message: "no such user found" });
+      }
+
+      await User.deleteOne({ emailId });
+      return res.status(200).json({ message: "user deleted sucessfully" });
+    }
+    return res.status(401).json({ message: "emailId provided was undefined" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Server Error ",
+    });
   }
-);
+});
 Router.post("/accept", AcceptValidator, adminVerify, async (req, res) => {
   try {
     const { isAccepted, activityfeedId } = req.body;
