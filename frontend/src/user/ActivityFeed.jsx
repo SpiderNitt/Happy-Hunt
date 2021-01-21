2d4d7ccfeadff9b3036f6f99b47a3675111f4de5
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-21 17:17:07 +0530

    integ

--- frontend/src/user/ActivityFeed.jsx
+++ frontend/src/user/ActivityFeed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -9,7 +9,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Avatar, CardHeader, IconButton } from '@material-ui/core';
 import { Favorite, PeopleAltOutlined, Share } from '@material-ui/icons';
-
+import client from '../api/client';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -32,25 +32,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Album() {
   const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await client.get('api/activity/feed')
+      setData(result.data.activityFeeds);
+  }
+    fetch();
+   
+}, []);
+console.log(data)
 
   return (
     <Container className={classes.cardGrid} maxWidth="md">
       <Grid container spacing={4}>
-        {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
-            <Card className={classes.card} item xs={3}>
+      {data.map((activity, index) => (
+        <Grid item key={activity._id} index={index + 1} xs={12} sm={6} md={4}>
+           <Card className={classes.card} item xs={3}>
                 <CardHeader
                     avatar={
                     <Avatar >
                         <PeopleAltOutlined />
                     </Avatar>
                     }
-                    title="Team D"
-                    subheader="35 mins ago"
+                    title={"Team : " + activity.TeamName}
+                    subheader= {(new Date(activity.Date)).getHours() +" : "+ (new Date(activity.Date)).getMinutes()}
                 />
                 <CardMedia
                     className={classes.cardMedia}
@@ -61,20 +70,26 @@ export default function Album() {
                 <Typography variant="body2" color="textSecondary" component="p">
                     #tag #tag2 #yipee 
                 </Typography>
+                <Typography variant="body1" color="textPrimary" component="p" style={{fontSize: 14, color:"gray", fontWeight:700 }}>
+                  {"activity : " + activity.activityName}
+                </Typography>
                 </CardContent>
                 <CardActions style={{display: 'flex', justifyContent: 'space-between' }}>
                   <IconButton>
-                      <Favorite />
+                      <Favorite /> <span style={{marginTop:3,marginLeft:5, fontSize: 18 }}>{activity.likes}</span>
                   </IconButton>
                   <IconButton>
                       <Share />
                   </IconButton>
                 </CardActions>
             </Card>
-          </Grid>
-        ))}
+        </Grid>
+        
+    ))}
       </Grid>
-    </Container>
+    
+     
+  </Container>
   );
 }
 
