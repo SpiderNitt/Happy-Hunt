0ec809b0e52e06138e63b9935f0ffc955f35fc12
paarthibank <63107117+paarthibank@users.noreply.github.com>
2021-01-23 01:31:03 +0530

    Made a time consuming working algorithm

--- src/helpers/timer.js
+++ src/helpers/timer.js
@@ -28,12 +28,11 @@ const BonusAsync = async () => {
   });
 };
 
-const missionGenerator = async (points, teams, category) => {
+const missionGenerator = async (points, teams, category, idList) => {
   let result = null;
   let lat = 0;
   let long = 0;
-  while (!result) {
-    console.log(lat, long, result);
+  while (result == null) {
     lat += 0.009;
     long += 0.00947;
     const lattop = teams.avgLocation.Lat + lat;
@@ -52,12 +51,12 @@ const missionGenerator = async (points, teams, category) => {
       },
       "assignedTeams.9": { $exists: false },
       Category: { $nin: category },
+      _id: { $nin: idList },
     });
   }
   return result;
 };
 // 0.009 latitude =1km 0.00947 longitude =1 km
-
 BonusAsync();
 setInterval(async () => {
   console.log("started");
@@ -65,39 +64,38 @@ setInterval(async () => {
   const teams = await Team.find({});
   // mission distribution
   for (let index = 0; index < teams.length; index += 1) {
-    let mission1;
-    let mission2;
-    let mission3;
     const category = [];
-    mission1 = await missionGenerator(100, teams[index], category);
-    console.log(mission1);
-    while (teams[index].assignedMissions.includes(mission1._id))
-      mission1 = await missionGenerator(100, teams[index], category);
+    const mission1 = await missionGenerator(
+      100,
+      teams[index],
+      category,
+      teams[index].assignedMissions
+    );
     teams[index].assignedMissions.push(mission1._id);
     await teams[index].save();
     category.push(mission1.Category);
-    mission2 = await missionGenerator(100, teams[index], category);
-    while (teams[index].assignedMissions.includes(mission2._id))
-      mission2 = await missionGenerator(100, teams[index], category);
+    const mission2 = await missionGenerator(
+      100,
+      teams[index],
+      category,
+      teams[index].assignedMissions
+    );
     teams[index].assignedMissions.push(mission2._id);
     await teams[index].save();
     category.push(mission2.Category);
-    mission3 = await missionGenerator(200, teams[index], category);
-    while (teams[index].assignedMissions.includes(mission3._id))
-      mission3 = await missionGenerator(200, teams[index], category);
-    console.log(mission1);
+    const mission3 = await missionGenerator(
+      200, // there are no 200 points mission
+      teams[index],
+      category,
+      teams[index].assignedMissions
+    );
     teams[index].assignedMissions.push(mission3._id);
     await teams[index].save();
-  }
-
-  // bonus distribution
-  for (let index = 0; index < teams.length; index += 1) {
     teams[index].assignedBonus.push(BonusMission100[counter]._id);
     teams[index].assignedBonus.push(BonusMission200[counter]._id);
     await teams[index].save();
   }
   counter += 1;
-  console.log("completed");
-}, 5000);
+}, 1800000);
 
 module.exports = { app, io };
