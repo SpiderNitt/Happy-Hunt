0e58149fd3431aec1a0c2b676b014590168632f1
Sai <saisandhya187@gmail.com>
2021-01-22 22:14:07 +0530

    Add loaders for mission component

--- frontend/src/admin/Mission.jsx
+++ frontend/src/admin/Mission.jsx
@@ -2,21 +2,29 @@ import React, { useEffect, useState } from 'react';
 import MissionCard from './MissionCard'
 import client from '../api/client';
 import { Link } from 'react-router-dom';
+import LoadingPage from '../components/LoadingPage';
 
 const Mission = () => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const fetchData = async () => {
             const result = await client.get('api/admin/mission')
             setData(result.data.Missions);
+            setLoading(false);
         }
         fetchData();
     }, []);
     return (
-        <div className="mission-container">
-            {data.map((mission, index) => (
-                <Link to={`/admin/missiondetail/${mission._id}`}><MissionCard key={mission._id} values={mission} index={index + 1} /></Link>
-            ))}
+        <div>
+            {loading && <LoadingPage />}
+            {!loading &&
+                <div className="mission-container">
+                    {!loading && data.map((mission, index) => (
+                        <Link to={`/admin/missiondetail/${mission._id}`}><MissionCard key={mission._id} values={mission} index={index + 1} /></Link>
+                    ))}
+                </div>
+            }
         </div>
     );
 }
