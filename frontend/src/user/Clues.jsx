eb52a386019ffb070cd14a3c7bab021261b5e01c
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-22 12:06:58 +0530

    integ (#102)
    * integ
    
    * add activity feed integ
    
    * minor cahnges

--- frontend/src/user/Clues.jsx
+++ frontend/src/user/Clues.jsx
@@ -1,67 +1,98 @@
-import React from 'react';
+import {React, useEffect, useState, createRef} from 'react';
+import client from '../api/client';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import clueData from './ClueData';
 import { Chip, Container, Grid } from '@material-ui/core';
-
-const RenderClues=(clue , index)=> {
-   const classes= useStyles();
-   
-    return (
-      <Card key={index} style={{ marginBottom: 10, padding: 10 }}>
-        <CardContent>
-          <div className={classes.container}>
-            <Typography className={classes.title} color="textSecondary" variant="ul">
-              Clue : {clue.cluename}
-            </Typography>
-            
-            <Chip size="small" label={clue.isSolved? "solved" : "unsolved" } />
-          </div>
-        </CardContent>
-        <CardActions style={{ display: 'flex', justifyContent: 'space-between'}}>
-          <Button variant="contained" size="small" href="/photo-clue">View</Button>
-          <Typography color="textSecondary">
-            {clue.points}
-          </Typography>
-        </CardActions>
-      </Card>
-    );
-}
-
-function Clues() {
-    return (
-      <Container maxWidth="md">
-        <Grid item xs={12}>
-          {clueData.map(RenderClues)}
-        </Grid>
-      </Container>
-    );
-}
-
-
-export default Clues;
+import Routes from '../utils/routes';
 
 const useStyles = makeStyles({
-    title: {
-      fontSize: 20,
-      color:"olive",
-      fontWeight: 'bold',
-      textAlign: 'left',
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    points: {
-      float:"right",
-    },
-    container: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginBottom: 10,
+   title: {
+     fontSize: 20,
+     color:"olive",
+     fontWeight: 300,
+     textAlign: 'left',
+     fontFamily:"tahoma"
+   },
+   pos: {
+     marginBottom: 12,
+   },
+   category: {
+    fontFamily:"tahoma",
+    fontStyle:"italic",
+    color:"gray",
+    float:"left",
+    marginLeft:10
+  },
+   points: {
+     float:"right",
+   },
+   container: {
+     display: 'flex',
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     marginBottom: 10,
+   }
+ });
+
+const RedirectUrl=(category)=>{
+  let url;
+  if(category=="Picture and Location"){
+    url=Routes.USER_LOCATION_CLUE
+  }
+  else if(category=="Text"){
+    url=Routes.USER_TEXT_CLUE
+  }
+  else if(category=="Picture" || category=="Video"){
+    url=Routes.USER_PICTURE_CLUE
+  }
+  return url;
+}
+
+function Clues(){
+  const classes= useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+        const result = await client.get('api/player/mission')
+        setData(result.data.missions);
+        
     }
-  });
+    fetch();
+   
+}, []);
+console.log(data)
+
+  return (   
+    <Container maxWidth="md">
+      {data.map((mission, index) => (
+          <Card key={mission._id} index={index + 1} style={{ marginBottom: 10, padding: 10 }}>
+       <CardContent>
+         <div className={classes.container}>
+           <Typography className={classes.title} color="textSecondary" variant="ul" >
+             {mission.clue}
+           </Typography>
+           <Chip size="small" label={data.isSolved? "solved" : "unsolved" } />
+         </div>
+       </CardContent>
+       <div className={classes.category} color="textSecondary" variant="p" >
+             {mission.Category}
+        </div>
+        <br/><br/>
+       <CardActions style={{ display: 'flex', justifyContent: 'space-between'}}>
+         <Button variant="contained" size="small" href={RedirectUrl(mission.Category)}>View</Button>
+         <Typography color="textSecondary">
+           {mission.maxPoints}
+         </Typography>
+       </CardActions>
+     </Card>
+      ))}
+      
+    </Container>
+  )
+};
+
+export default Clues;
