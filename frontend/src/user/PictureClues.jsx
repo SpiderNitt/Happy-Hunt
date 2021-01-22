58694d24f5f57c967a19e1ed24b0bcd2655ef6e0
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-22 18:26:38 +0530

    changes in clue detials template

--- frontend/src/user/PictureClues.jsx
+++ frontend/src/user/PictureClues.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { useState } from "react";
 import Container from '@material-ui/core/Container';
 import Sample3 from '../assets/sample3.jpg';
 import { create } from 'apisauce';
@@ -10,6 +9,8 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import Hints from './Hints';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Routes from '../utils/routes';
 
 const api = create({
     baseURL: 'https://api.cloudinary.com/v1_1/dqj309mtu/image',
@@ -20,6 +21,26 @@ function PictureClues(clue, index) {
     const [dataUri, setDataUri] = useState('');
     const [imageSelected, setImageSelected] = useState("");
     const [open, setOpen] = useState(false);
+    const [location, setLocation]= useState({
+        loaded:false,
+        coordinates: {lat:"", long:""}
+    });
+
+    const onSucces = location=>{
+        setLocation({
+            loaded:true,
+            coordinates:{
+                lat: location.coords.latitude,
+                long: location.coords.longitude
+            }
+        })
+    };
+
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(onSucces);
+
+    }, []);
+    console.log(location);
 
     const handleOpen = () => {
       setOpen(true);
@@ -42,14 +63,14 @@ function PictureClues(clue, index) {
     return (
         
         <React.Fragment >
-            <Container maxWidth="sm" style={{ backgroundColor: '#484848', height: '100vh', marginTop:"10vh" }}>
+            <Container maxWidth="sm" style={{ height: '100vh', marginTop:"10vh" }}>
                 <h4 style={{color:'#57CFEF',
                     fontSize:25,
                     fontFamily:'tahoma', display:'flex', alignItems:'center', justifyContent:'center', paddingTop:12}}>
-                        { clueData[0].cluename}
+                       Mission : { clueData[0].cluename}
                 </h4>
-                <p style={{color:'#F5F5F5',
-                    fontSize:16,
+                <p style={{color:'dark-gray',
+                    fontSize:20,
                     fontFamily:'calibri',
                     display:'flex', alignItems:'center', justifyContent:'center'}}>What to do:<span>Solve the below riddle and reach the spot within 100mts to score points.</span></p>
                     <div style= {{display:'flex', alignItems:'center', justifyContent:'center' }}>
@@ -62,6 +83,9 @@ function PictureClues(clue, index) {
                     </p>
                     <p className={classes.points}>100 points</p>
                 </div>
+                <div>
+                    <LocationOnIcon className={classes.icon} />
+                </div>
 
                <input type="file" onChange={(e)=>{
                     setImageSelected(e.target.files[0]);
@@ -70,8 +94,8 @@ function PictureClues(clue, index) {
                 <Button type="submit" onClick={uploadImage}  style={{backgroundColor:"#4863A0", color:"whitesmoke"}}>
                     Submit
                 </Button>
-                <p style={{fontSize:12, fontStyle: 'italic',fontFamily:'tahoma', color:"whitesmoke", display:'flex', justifyContent:'center'}}>note: the picture should be taken from inside the car.</p>
-                <Button className={classes.Button} href="/clue">Back to clues</Button>
+                <p style={{fontSize:12, fontStyle: 'italic',fontFamily:'tahoma', color:"black", display:'flex', justifyContent:'center'}}>note: the picture should be taken from inside the car.</p>
+                <Button className={classes.Button} href={Routes.USER_CLUES}>Back to clues</Button>
                 <Button className={classes.Button} href="/photo">Take Picture!</Button>
                 <Button className={classes.Button} onClick={handleOpen} >Hint</Button>
                 <Modal
@@ -86,11 +110,11 @@ function PictureClues(clue, index) {
                 timeout: 500,
                 }}
             >
-                <Fade in={open}>
+<Fade in={open}>
                 <div className={classes.paper}>
                     <Hints/>
                 </div>
-                </Fade>
+                </Fade>                
             </Modal>
                 
             </Container>
@@ -104,8 +128,8 @@ const useStyles = makeStyles((theme)=>({
       display:'flex',
       alignSelf:'center',
       justifyContent:'center',
-      color:'white',
-      fontSize:20,
+      color:'gray',
+      fontSize:16,
       fontFamily:'tahoma'
     },
     button: {
@@ -141,6 +165,10 @@ const useStyles = makeStyles((theme)=>({
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
       },
+      icon: {
+        fontSize:65,
+        color:"#EF7257"
+    }
   }));
 
 export default PictureClues;
--- frontend/src/user/TextClues.jsx
+++ frontend/src/user/TextClues.jsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
-
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import Hints from './Hints';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { TextareaAutosize } from '@material-ui/core';
-
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Routes from '../utils/routes';
 
 function TextClues(props) {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [location, setLocation]= useState({
+        loaded:false,
+        coordinates: {lat:"", long:""}
+    });
+
+    const onSucces = location=>{
+        setLocation({
+            loaded:true,
+            coordinates:{
+                lat: location.coords.latitude,
+                long: location.coords.longitude
+            }
+        })
+    };
+
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition(onSucces);
+
+    }, []);
+    console.log(location);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
         <React.Fragment >
             
-            <Container maxWidth="sm" style={{ backgroundColor: '#484848', height: '100vh', marginTop:"10vh" }}>
+            <Container maxWidth="sm" style={{ height: '100vh', marginTop:"10vh" }}>
                 <h4 style={{color:'#C866F5',
                     fontSize:25,
                     fontFamily:'tahoma', display:'flex', alignItems:'center', justifyContent:'center', paddingTop:12}}>
                         Clue: Chaos is my friend!
                 </h4>
-                <p style={{color:'#F5F5F5',
+                <p style={{color:'gray',
                     fontSize:16,
                     fontFamily:'calibri',
                     display:'flex', alignItems:'center', justifyContent:'center'}}>What to do: <span style={{marginLeft:5}}> Follow the instructions as given below</span>
@@ -40,17 +73,39 @@ function TextClues(props) {
                     <span  className={classes.points}>100 points</span>
                     </p>
                 </div>
+                <div>
+                    <LocationOnIcon className={classes.icon} />
+                </div>
 
-                <p style={{fontSize:12, fontStyle: 'italic',fontFamily:'tahoma', color:"whitesmoke", display:'flex', justifyContent:'center'}}>
+                <p style={{fontSize:12, fontStyle: 'italic',fontFamily:'tahoma', color:"dark-gray", display:'flex', justifyContent:'center'}}>
                     note: the picture should be taken from inside the car.
                 </p>
                 <form className={classes.root} noValidate autoComplete="off">
                 <TextareaAutosize style={{fontSize:15, padding:12, minHeight:20, maxWidth:300}} placeholder="Answer" required/>
                 </form>
-                <Button className={classes.Button} href="/clue">Back to clues</Button>
+                <Button className={classes.Button} href={Routes.USER_CLUES}>Back to clues</Button>
                 <Button className={classes.Button}  href="/photo">Take Picture!</Button>
-                <Button className={classes.Button}>Hint</Button>
+                <Button className={classes.Button} onClick={handleOpen}>Hint</Button>
                 
+                <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                <div className={classes.paper}>
+                    <Hints/>
+                </div>
+                </Fade>
+            </Modal>
+
             </Container>
         </React.Fragment>
       );
@@ -69,7 +124,7 @@ const useStyles = makeStyles((theme)=>({
       display:'flex',
       alignSelf:'center',
       justifyContent:'center',
-      color:'white',
+      color:'gray',
       fontSize:20,
       fontFamily:'tahoma'
     },
@@ -77,11 +132,17 @@ const useStyles = makeStyles((theme)=>({
         display:'flex',
         alignSelf:'center',
         justifyContent:'center',
-        color:'gray',
+        color:'dark-gray',
         fontSize:18,
         fontFamily:'tahoma'
-      },
-      points: {
+    },
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        
+    },
+    points: {
         float:"right", 
         fontStyle:"italic",
         fontSize: 16, 
@@ -94,11 +155,23 @@ const useStyles = makeStyles((theme)=>({
         color:'white',
         fontSize:20,
         fontFamily:'tahoma'
-      },
-      Button:{color:'white', 
+    },
+    Button:
+      {color:'white', 
       fontFamily:'tahoma', 
       backgroundColor:"gray",
-      margin:5}
+      margin:5
+     },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
+    icon: {
+        fontSize:65,
+        color:"#EF7257"
+    }
     }));
 
 export default TextClues;
