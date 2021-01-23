154dfced29bd956ae06f1a69c1fa4582f3f31187
Sai <saisandhya187@gmail.com>
2021-01-23 17:14:58 +0530

    Integ part for fetching clues

--- frontend/src/user/ClueTabs.jsx
+++ frontend/src/user/ClueTabs.jsx
@@ -1,20 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { Lock, LockOpen } from '@material-ui/icons';
 import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+import Clues from './Clues';
+import client from '../api/client';
 
 const useStyles = makeStyles({
   root: {
-      marginTop: 5,
+    marginTop: 5,
   },
 });
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 export default function ClueTabs() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-
+  const [open1, setOpen1] = useState(true);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [data, setData] = useState([]);
+  const fetch = async () => {
+    const result = await client.get('api/player/mission')
+    if (!result.ok) {
+      console.log(result.originalError, result.problem, result.status);
+      return;
+    }
+    console.log(result.data);
+    if (result.data.missions.length > 3) {
+      setOpen2(true);
+    }
+    if (result.data.missions.length > 6) {
+      setOpen3(true);
+    }
+    setData(result.data.missions);
+  }
+  useEffect(() => {
+    fetch();
+  }, []);
+  const getSet1 = () => {
+    if (data.length >= 3) {
+      return data.slice(0, 3);
+    }
+    else {
+      return [];
+    }
+  }
+  const getSet2 = () => {
+    if (data.length >= 6) {
+      return data.slice(3, 6);
+    }
+    else {
+      return [];
+    }
+  }
+  const getSet3 = () => {
+    if (data.length >= 9) {
+      return data.slice(6, 9);
+    }
+    else {
+      return [];
+    }
+  }
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -28,10 +111,19 @@ export default function ClueTabs() {
         indicatorColor="secondary"
         textColor="secondary"
       >
-        <Tab icon={<LockOpen />} />
-        <Tab icon={<Lock />} disabled />
-        <Tab icon={<Lock />} disabled />
+        {open1 ? <Tab icon={<LockOpen {...a11yProps(0)} />} /> : <Tab icon={<Lock />} />}
+        {open2 ? <Tab icon={<LockOpen {...a11yProps(1)} />} /> : <Tab icon={<Lock />} disabled />}
+        {open3 ? <Tab icon={<LockOpen {...a11yProps(2)} />} /> : <Tab icon={<Lock />} disabled />}
       </Tabs>
+      <TabPanel value={value} index={0}>
+        <Clues data={getSet1()} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Clues data={getSet2()} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Clues data={getSet3()} />
+      </TabPanel>
     </Container>
   );
 }
--- frontend/src/user/Clues.jsx
+++ frontend/src/user/Clues.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect, useState, createRef} from 'react';
+import { React, useEffect, useState, createRef } from 'react';
 import client from '../api/client';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -10,88 +10,76 @@ import { Chip, Container, Grid } from '@material-ui/core';
 import Routes from '../utils/routes';
 
 const useStyles = makeStyles({
-   title: {
-     fontSize: 20,
-     color:"olive",
-     fontWeight: 300,
-     textAlign: 'left',
-     fontFamily:"tahoma"
-   },
-   pos: {
-     marginBottom: 12,
-   },
-   category: {
-    fontFamily:"tahoma",
-    fontStyle:"italic",
-    color:"gray",
-    float:"left",
-    marginLeft:10
+  title: {
+    fontSize: 20,
+    color: "olive",
+    fontWeight: 300,
+    textAlign: 'left',
+    fontFamily: "tahoma"
   },
-   points: {
-     float:"right",
-   },
-   container: {
-     display: 'flex',
-     flexDirection: 'row',
-     justifyContent: 'space-between',
-     marginBottom: 10,
-   }
- });
+  pos: {
+    marginBottom: 12,
+  },
+  category: {
+    fontFamily: "tahoma",
+    fontStyle: "italic",
+    color: "gray",
+    float: "left",
+    marginLeft: 10
+  },
+  points: {
+    float: "right",
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  }
+});
 
-const RedirectUrl=(category)=>{
+const RedirectUrl = (category) => {
   let url;
-  if(category=="Picture and Location"){
-    url=Routes.USER_LOCATION_CLUE
+  if (category == "Picture and Location") {
+    url = Routes.USER_LOCATION_CLUE
   }
-  else if(category=="Text"){
-    url=Routes.USER_TEXT_CLUE
+  else if (category == "Text") {
+    url = Routes.USER_TEXT_CLUE
   }
-  else if(category=="Picture" || category=="Video"){
-    url=Routes.USER_PICTURE_CLUE
+  else if (category == "Picture" || category == "Video") {
+    url = Routes.USER_PICTURE_CLUE
   }
   return url;
 }
 
-function Clues(){
-  const classes= useStyles();
-  const [data, setData] = useState([]);
-  const fetch = async () => {
-    const result = await client.get('api/player/mission')
-    if(!result.ok){
-      console.log(result.originalError, result.problem, result.status);
-      return;
-    }
-    setData(result.data.missions);
-  }
-  useEffect(() => {
-    fetch();
-  }, []);
-
-  return (   
+function Clues(props) {
+  const classes = useStyles();
+  const { data } = props;
+  return (
     <Container maxWidth="md">
       {data !== [] && data.map((mission, index) => (
-          <Card key={mission._id} index={index + 1} style={{ marginBottom: 10, padding: 10 }}>
-       <CardContent>
-         <div className={classes.container}>
-           <Typography className={classes.title} color="textSecondary" variant="ul" >
-             {mission.clue}
-           </Typography>
-           <Chip size="small" label={data.isSolved? "solved" : "unsolved" } />
-         </div>
-       </CardContent>
-       <div className={classes.category} color="textSecondary" variant="p" >
-             {mission.Category}
-        </div>
-        <br/><br/>
-       <CardActions style={{ display: 'flex', justifyContent: 'space-between'}}>
-         <Button variant="contained" size="small" href={RedirectUrl(mission.Category)}>View</Button>
-         <Typography color="textSecondary">
-           {mission.maxPoints}
-         </Typography>
-       </CardActions>
-     </Card>
+        <Card key={mission._id} index={index + 1} style={{ marginBottom: 10, padding: 10 }}>
+          <CardContent>
+            <div className={classes.container}>
+              <Typography className={classes.title} color="textSecondary" variant="ul" >
+                {mission.clue}
+              </Typography>
+              <Chip size="small" label={data.isSolved ? "solved" : "unsolved"} />
+            </div>
+          </CardContent>
+          <div className={classes.category} color="textSecondary" variant="p" >
+            {mission.Category}
+          </div>
+          <br /><br />
+          <CardActions style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button variant="contained" size="small" href={RedirectUrl(mission.Category)}>View</Button>
+            <Typography color="textSecondary">
+              {mission.maxPoints}
+            </Typography>
+          </CardActions>
+        </Card>
       ))}
-      
+
     </Container>
   )
 };
