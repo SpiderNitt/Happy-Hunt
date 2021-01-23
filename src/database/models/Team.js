7de8e2753113030475882ed991fedd79a746e7f4
paarthibank <63107117+paarthibank@users.noreply.github.com>
2021-01-23 08:39:06 +0530

    Rebased

--- src/database/models/Team.js
+++ src/database/models/Team.js
@@ -56,7 +56,10 @@ const TeamSchema = new mongoose.Schema({
       type: Number,
     },
   },
-
+  maxPointsAssigned: {
+    type: Number,
+    default: 0,
+  },
   Notifications: [{ type: String }],
 });
 module.exports = mongoose.model("Team", TeamSchema);
