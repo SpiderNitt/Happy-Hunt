2bab574583a2a54e59b49394fcde30d0278293ed
unknown <mansibisen354@gmail.com>
2021-01-26 10:52:45 +0530

    email setup with removal of status 500 user err

---
+++ src/helpers/EMAIL/nodemailer.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (sender, subject, text, html) => {
+  try {
+    const testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: testAccount.user, // generated ethereal user
+        pass: testAccount.pass, // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: sender, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (e) {
+    console.log(e);
+  }
+};
+module.exports = { sendEmail };
