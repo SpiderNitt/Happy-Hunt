b81481582f1613c571065ff2a78bd18809347c1f
Sai <saisandhya187@gmail.com>
2021-01-26 20:28:54 +0530

    Update Mission detail page with all field names

--- frontend/src/admin/MissionListItem.jsx
+++ frontend/src/admin/MissionListItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -8,17 +8,43 @@ const useStyles = makeStyles((theme) => ({
     },
     bold: {
         fontWeight: 'bold'
+    },
+    italic: {
+        fontStyle: 'italic'
+    },
+    img: {
+        width: '300px',
+        height: '300px',
     }
 }))
 
 
 function MissionListItem(props) {
     const classes = useStyles();
+    useEffect(() => {
+        if (props.eval) {
+            console.log(props.value);
+        }
+    })
     return (
         <div className={classes.root}>
-            <p><span className={classes.bold}>{props.title}</span> {props.value}</p>
+            {props.eval && props.value.length === 3 && <div>
+                <p className={classes.bold}>{props.title}</p>
+                <p><span className={classes.italic}>Part A:</span> {props.value[0]}</p>
+                <p><span className={classes.italic}>Part B:</span> {props.value[1]}</p>
+                <img src={props.value[2]} alt='picture clue' className={classes.img} />
+            </div>}
+            {props.eval && props.value.length === 1 && <div>
+                <p className={classes.bold}>{props.title}</p>
+                <p><span className={classes.italic}>Part A:</span> {props.value[0]}</p>
+            </div>}
+            {!props.eval && <p><span className={classes.bold}>{props.title}</span> {props.value}</p>}
         </div>
     )
+}
+
+MissionListItem.defaultProps = {
+    eval: false
 }
 
 export default MissionListItem;
