22b55b0c08f1eae7648d16ac02b156cc5f4e94eb
Sai <saisandhya187@gmail.com>
2021-01-27 21:36:43 +0530

    Add support for image upload while adding mission

--- frontend/src/admin/NewMission.jsx
+++ frontend/src/admin/NewMission.jsx
@@ -1,30 +1,67 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { useFormik, Field } from 'formik';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
 import client from '../api/client';
 import { withRouter } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
+const categories = ['Riddles', 'Cryptic', 'Graphic', 'Factoids', 'Bonus'];
+const answerTypes = ['Picture', 'Video', 'Picture and Location', 'Text']
 
 const NewMission = (props) => {
     const { history } = props;
+    const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file)
+            fileReader.onload = () => {
+                formik.setFieldValue('file', fileReader.result);
+                resolve(fileReader.result);
+            }
+            fileReader.onerror = (error) => {
+                reject(error);
+            }
+        })
+    }
+    const handleImageUpload = async (event) => {
+        const file = event.target.files[0]
+        await convertBase64(file);
+    }
     const formik = useFormik({
         initialValues: {
             Category: '',
-            clue: '',
+            clue1: '',
+            clue2: '',
             answer: '',
             Other_Info: '',
             answer_Type: '',
+            ServerEvaluation: ''
         },
         onSubmit: async (values) => {
-            const { Category, clue, answer, answer_Type, Other_Info, Lat, Long, maxPoints, MissionName } = values;
+            const { Category, clue2, answer, answer_Type, Other_Info, Lat, Long, maxPoints, MissionName, ServerEvaluation, clue1, file } = values;
             const answerArray = [];
             answerArray.push(answer);
+            const cluesArray = [];
+            if (clue2 === "") {
+                cluesArray.push(clue1);
+            }
+            else {
+                cluesArray.push(clue1);
+                if (values.file !== undefined) {
+                    cluesArray.push(clue2);
+                    cluesArray.push(file);
+                }
+            }
             const object = {
                 Category,
-                clue,
+                clue: cluesArray,
                 answer: answerArray,
                 answer_Type,
                 Other_Info,
@@ -34,7 +71,7 @@ const NewMission = (props) => {
                 },
                 MissionName,
                 Feed: true,
-                ServerEvaluation: false,
+                ServerEvaluation,
                 maxPoints,
                 Hints: [
                     {
@@ -49,8 +86,9 @@ const NewMission = (props) => {
                     }
                 ]
             };
-            const response = await client.post('api/admin/mission/add', object);
-            console.log(response);
+            console.log(object)
+            // const response = await client.post('api/admin/mission/add', object);
+            // console.log(response);
         },
     });
 
@@ -69,7 +107,7 @@ const NewMission = (props) => {
                 left: '50%',
                 top: '50%',
                 transform: 'translate(-50%, -50%)',
-                marginTop: '10px',
+                marginTop: '100px',
                 width: '600px'
             }}>
                 <h3 style={{ textAlign: 'center' }}>Add a Mission</h3>
@@ -79,11 +117,18 @@ const NewMission = (props) => {
                         id="Category"
                         name="Category"
                         label="Category"
+                        select
                         value={formik.values.Category}
                         onChange={formik.handleChange}
                         error={formik.touched.Category && Boolean(formik.errors.Category)}
                         helperText={formik.touched.Category && formik.errors.Category}
-                    />
+                    >
+                        {categories.map((category, index) => (
+                            <MenuItem key={index} value={category}>
+                                {category}
+                            </MenuItem>
+                        ))}
+                    </TextField>
                     <TextField
                         fullWidth
                         id="MissionName"
@@ -96,13 +141,23 @@ const NewMission = (props) => {
                     />
                     <TextField
                         fullWidth
-                        id="clue"
-                        name="clue"
-                        label="Clue"
-                        value={formik.values.clue}
+                        id="clue1"
+                        name="clue1"
+                        label="Clue Part-A"
+                        value={formik.values.clue1}
                         onChange={formik.handleChange}
-                        error={formik.touched.clue && Boolean(formik.errors.clue)}
-                        helperText={formik.touched.clue && formik.errors.clue}
+                        error={formik.touched.clue1 && Boolean(formik.errors.clue1)}
+                        helperText={formik.touched.clue1 && formik.errors.clue1}
+                    />
+                    <TextField
+                        fullWidth
+                        id="clue2"
+                        name="clue2"
+                        label="Clue Part-B"
+                        value={formik.values.clue2}
+                        onChange={formik.handleChange}
+                        error={formik.touched.clue2 && Boolean(formik.errors.clue2)}
+                        helperText={formik.touched.clue2 && formik.errors.clue2}
                     />
                     <TextField
                         fullWidth
@@ -114,20 +169,37 @@ const NewMission = (props) => {
                         error={formik.touched.answer && Boolean(formik.errors.answer)}
                         helperText={formik.touched.answer && formik.errors.answer}
                     />
-                    <Select
-                        onChange={formik.handleChange}
+                    <TextField
+                        fullWidth
                         id="answer_Type"
-                        name='answer_Type'
+                        name="answer_Type"
                         label="Answer Type"
+                        select
                         value={formik.values['answer_Type']}
+                        onChange={formik.handleChange}
                         error={formik.touched['answer_Type'] && Boolean(formik.errors['answer_Type'])}
                         helperText={formik.touched['answer_Type'] && formik.errors['answer_Type']}
-                        fullWidth
                     >
-                        <option value='picture'>Picture</option>
-                        <option value='location'>Location</option>
-                        <option value='text'>Text</option>
-                    </Select>
+                        {answerTypes.map((type, index) => (
+                            <MenuItem key={index} value={type}>
+                                {type}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Server Evaluation</FormLabel>
+                        <RadioGroup aria-label="ServerEvaluation" name="ServerEvaluation" value={formik.values['ServerEvaluation']} onChange={formik.handleChange} row>
+                            <FormControlLabel value='True' control={<Radio />} label="YES" />
+                            <FormControlLabel value='False' control={<Radio />} label="NO" />
+                        </RadioGroup>
+                    </FormControl>
+                    <br />
+                    <div className="form-group">
+                        <FormLabel component="legend">Image Upload</FormLabel>
+                        <input type="file" onChange={(event) => {
+                            handleImageUpload(event);
+                        }} />
+                    </div>
                     <TextField
                         fullWidth
                         id="Lat"
@@ -137,6 +209,7 @@ const NewMission = (props) => {
                         onChange={formik.handleChange}
                         error={formik.touched.Lat && Boolean(formik.errors.Lat)}
                         helperText={formik.touched.Lat && formik.errors.Lat}
+
                     />
                     <TextField
                         fullWidth
@@ -148,6 +221,7 @@ const NewMission = (props) => {
                         error={formik.touched.Long && Boolean(formik.errors.Long)}
                         helperText={formik.touched.Long && formik.errors.Long}
                     />
+
                     <TextField
                         fullWidth
                         id="Other_Info"
@@ -171,7 +245,7 @@ const NewMission = (props) => {
                     />
                     <Button color="primary" variant="contained" fullWidth type="submit">
                         Submit
-        </Button>
+                    </Button>
                 </form>
             </div>
         </div>
