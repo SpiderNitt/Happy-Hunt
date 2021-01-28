1ac1335a4987b566c79c142b77e70915d9f96880
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-01-28 18:31:41 +0530

    wip

--- frontend/src/user/LocationClues.jsx
+++ frontend/src/user/LocationClues.jsx
@@ -8,14 +8,28 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import Hints from './Hints';
+import client from '../api/client'
 
 function LocationClues(props) {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [data, setData]= useState([]);
     const [location, setLocation]= useState({
         loaded:false,
         coordinates: {lat:"", long:""}
     });
+
+    const fetch = async () => {
+        const result = await client.get(`api/mission/${props.match.params.id}`);
+        console.log(result.data);
+        setData(result.data.mission);
+    }
+
+      useEffect(() => {
+        fetch();
+      }, [props.match.params.id]);
+
+      console.log(data)
 
     const onSucces = location=>{
         setLocation({
@@ -48,39 +62,46 @@ function LocationClues(props) {
                 <h4 style={{color:'#50EFE6',
                     fontSize:25,
                     fontFamily:'tahoma', display:'flex', alignItems:'center', justifyContent:'center', paddingTop:12}}>
-                        Mission : Let's pass by!
+                        Mission : {data.MissionName}
                 </h4>
                 <p style={{color:"black",
                     fontSize:16,
                     fontFamily:'calibri',
-                    display:'flex', alignItems:'center', justifyContent:'center'}}>What to do: <span style={{marginLeft:5}}> Follow the instructions as given below</span>
+                    display:'flex', alignItems:'center', justifyContent:'center'}}>
+                        {data.Other_Info} 
                 </p>
-                <div className={classes.task1}>
+                {/* <div className={classes.task1}>
                    <p>
-                    "If you got a sweet tooth, 
-                     <br/>  I can tell you where to come 
-                    <br/> We are here at the east street,
-                    <br/> Just follow the biscuit crumbs."  
-                    
+                   {data.clue[0]}
                     </p>
                 </div>
-                <p className={classes.points}>100 points</p>
+                {(data.clue[1]) ? (
+                     <div className={classes.task1}>
+                     <p>
+                     {data.clue[1]}
+                      </p>
+                  </div>
+                ) :''}
+                 {(data.clue[2]) ? (
+                     <div className={classes.task1}>
+                     <p>
+                     {data.clue[2]}
+                      </p>
+                  </div>
+                ) :''} */}
+                <p className={classes.points}>{data.maxPoints}</p>
                 <br/>
                 <br/>
                 <br/>
                 <div>
                     <LocationOnIcon className={classes.icon} />
                 </div>
-                
-                {/* <p style={{fontSize:12, fontStyle: 'italic',fontFamily:'tahoma', color:"whitesmoke", display:'flex', justifyContent:'center'}}>
-                    { location ? JSON.stringify(location) : " Location data not available."}
-                </p> */}
                 <br/>
                 <br/>
                 <Button className={classes.Button} href={Routes.USER_CLUES}>Back to clues</Button>
                 <Button className={classes.Button}  href="/photo">Take Picture!</Button>
-                <Button className={classes.Button}  onClick={handleOpen}>Hint</Button>
-                
+                {!data.isBonus ? (<div>
+                    <Button className={classes.Button} onClick={handleOpen} >Hint</Button>
                 <Modal
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
@@ -92,13 +113,14 @@ function LocationClues(props) {
                 BackdropProps={{
                 timeout: 500,
                 }}
-            >
+                >
                 <Fade in={open}>
                 <div className={classes.paper}>
-                    <Hints/>
+                    <Hints id={data._id}/>
                 </div>
-                </Fade>
+                </Fade>                
                 </Modal>
+                </div>) :''}
                 </Container>
             </React.Fragment>
       );
