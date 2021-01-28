bb38f19d2122e2f6967564ccd0da418cf418b0fb
paarthibank <63107117+paarthibank@users.noreply.github.com>
2021-01-28 17:12:26 +0530

    Made changes in payment and all other routes

--- src/api/routers/openApi.js
+++ src/api/routers/openApi.js
@@ -1,10 +1,8 @@
 const openApi = require("express").Router();
-const { uid } = require("uid");
 const Team = require("../../database/models/Team");
 const Mission = require("../../database/models/Mission");
 const User = require("../../database/models/User");
 const Hint = require("../../database/models/Hint");
-const { sendEmail } = require("../../helpers/EMAIL/nodemailer");
 
 openApi.get("/scoreboard", async (req, res) => {
   try {
@@ -37,77 +35,6 @@ openApi.get("/mission/:id", async (req, res) => {
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({ Message: "Server Error, Try again later" });
-  }
-});
-openApi.post("/payment", async (req, res) => {
-  try {
-    const emailId = req.body.payload.payment.email;
-    const phoneNo = req.body.payload.payment.contact;
-    const { amount } = req.body.payload.payment;
-    let user = await User.findOne({ emailId });
-    if (!user) {
-      user = await User.findOne({ phoneNo });
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-    }
-    let result;
-    if (user.team) {
-      if (amount >= 399 && amount < 499)
-        result = await Team.updateOne({ _id: user.team }, { Paid: 4 });
-      else if (amount >= 499)
-        result = await Team.updateOne({ _id: user.team }, { Paid: 6 });
-    } else if (amount >= 399 && amount < 499)
-      result = await User.updateOne({ emailId }, { Paid: 4 });
-    else if (amount >= 499)
-      result = await User.updateOne({ emailId }, { Paid: 6 });
-    if (result.nModified === 1) {
-      return res.status(200).json({ message: "Success" });
-    }
-    return res.status(400).json({ message: "Unable to update record" });
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).json({ message: "Server Error, Try again later" });
-  }
-});
-openApi.post("/payment_1", async (req, res) => {
-  try {
-    const emailId = req.body.emailID;
-    const phoneNo = req.body.phoneNumber;
-    const quantity = req.body.totalTicketQuantity;
-    let user = await User.findOne({ emailId });
-    if (!user) {
-      user = await User.findOne({ phoneNo });
-      if (!user) {
-        const password = uid();
-        const user = await User.create({
-          emailId,
-          phoneNo,
-          password,
-          active: true,
-          Role: "Player",
-          Paid: quantity,
-        });
-        await sendEmail(
-          emailId,
-          "USER created",
-          `password:${password}`,
-          "<h1>hello</h1>"
-        );
-        return res.status(200).json({ message: "Success" });
-      }
-    }
-    let result;
-    if (user.team)
-      result = await Team.updateOne({ _id: user.team }, { Paid: quantity });
-    else result = await User.updateOne({ emailId }, { Paid: quantity });
-    if (result.nModified === 1) {
-      return res.status(200).json({ message: "Success" });
-    }
-    return res.status(400).json({ message: "Unable to update record" });
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).json({ message: "Server Error, Try again later" });
   }
 });
 openApi.get("/adminList", async (req, res) => {
