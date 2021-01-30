dcc7284489e1d8cd7c4693abf32b7e183e004a95
highonweb <mothish07122001@outlook.com>
2021-01-30 21:38:32 +0530

    Feat: Score Route done and deleted Inhott

--- src/api/routers/teamRouter.js
+++ src/api/routers/teamRouter.js
@@ -228,4 +228,14 @@ team.get("/requests", async (req, res) => {
     res.status(500).json({ message: e.message });
   }
 });
+team.get("/score", async (req, res) => {
+  try {
+    const score = await Team.findById(req.jwt_payload.team, { points: 1 });
+    return res.status(200).json({ score });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
 module.exports = team;
