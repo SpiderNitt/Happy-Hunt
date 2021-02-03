32a21c81d4106a625435e44effd0392942d093da
gokul-00 <62957549+gokul-00@users.noreply.github.com>
2021-02-03 23:23:39 +0530

    Frontend4 design (#177)
    * basic home page design
    
    * basic home page design
    
    * footer added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Verify from "./components/Verify";
+// import Footer from "./Footer";
 // import AdminMembers from "./AdminMembers";
 // import AdminRegistration from "./AdminRegistration";
- import App from "./App";
+import App from "./App";
+import Pricing from "./components/Pricing";
+// import SampleHomePage from "./SampleHomePage";
+// import StickyFooter from "./StickyFooter";
 // import Camera from "./components/Camera";
 // import Verify from "./components/Verify";
 // import WebShare from "./components/WebShare";
