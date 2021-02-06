a784524f74cdf11efd300a7d2952abf106c6d19f
gokul-00 <106119141@nitt.edu>
2021-02-06 19:21:24 +0530

    production code changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 // import AdminMembers from "./AdminMembers";
 // import AdminRegistration from "./AdminRegistration";
 import App from "./App";
-import Pricing from "./components/Pricing";
+// import Pricing from "./components/Pricing";
 // import SampleHomePage from "./SampleHomePage";
 // import StickyFooter from "./StickyFooter";
 // import Camera from "./components/Camera";
