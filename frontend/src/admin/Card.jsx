2a23abefd069d7de1cede62c927d0d70a66f9a6d
unknown <mansibisen354@gmail.com>
2021-02-06 12:35:21 +0530

    string to JSON conversion with other changes

--- frontend/src/admin/Card.jsx
+++ frontend/src/admin/Card.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import GroupIcon from '@material-ui/icons/Group';
-import { Button } from '@material-ui/core';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import client from '../api/client';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import GroupIcon from "@material-ui/icons/Group";
+import { Button } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import client from "../api/client";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,17 +24,16 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   avatar: {
     backgroundColor: red[500],
   },
   buttonGroup: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    flexDirection: "row",
+    justifyContent: "flex-end",
   },
 }));
-
 
 export default function ActivityFeedCard(props) {
   const [open, setOpen] = useState(false);
@@ -48,25 +47,27 @@ export default function ActivityFeedCard(props) {
   const handleAccept = (isAccepted, activityfeedId) => {
     console.log(isAccepted, activityfeedId);
     const object = {
-      isAccepted, activityfeedId
-    }
+      isAccepted,
+      activityfeedId,
+    };
     //route for accepting
-    const response = client.post('api/admin/accept', object);
+    const response = client.post("api/admin/accept", object);
     console.log(response);
-  }
+  };
   const handleReject = () => {
     handleOpen();
-  }
+  };
   const handleRejectConfirm = (isAccepted, activityfeedId) => {
     console.log(isAccepted, activityfeedId);
     //route for rejecting
     const object = {
-      isAccepted, activityfeedId
-    }
-    const response = client.post('api/admin/accept', object);
+      isAccepted,
+      activityfeedId,
+    };
+    const response = client.post("api/admin/accept", object);
     console.log(response);
     handleClose();
-  }
+  };
   return (
     <Card className={classes.root}>
       <CardHeader
@@ -75,45 +76,58 @@ export default function ActivityFeedCard(props) {
             <GroupIcon />
           </Avatar>
         }
-        title={`Team id: ${props.data.team}`}
+        title={`Team id: ${props.data.team.teamName}`}
         subheader={props.data.Date}
       />
       <CardMedia
         className={classes.media}
         image={props.data.Answer}
-        title="image"
+        title='image'
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {`Mission id: ${props.data.mission}`}
+        <Typography variant='body2' color='textSecondary' component='p'>
+          {`Mission id: ${props.data.mission.MissionName}`}
         </Typography>
       </CardContent>
       <CardActions className={classes.buttonGroup}>
-        <Button variant="contained" color="primary" style={{ marginRight: 10 }} onClick={() => handleAccept(true, props.data['_id'])}>
+        <Button
+          variant='contained'
+          color='primary'
+          style={{ marginRight: 10 }}
+          onClick={() => handleAccept(true, props.data["_id"])}>
           Accept
         </Button>
-        <Button variant="contained" color="secondary" style={{ marginRight: 10 }} onClick={handleReject}>
+        <Button
+          variant='contained'
+          color='secondary'
+          style={{ marginRight: 10 }}
+          onClick={handleReject}>
           Reject
         </Button>
         <Dialog
           open={open}
           onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{"REJECT SUBMISSION"}</DialogTitle>
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'>
+          <DialogTitle id='alert-dialog-title'>
+            {"REJECT SUBMISSION"}
+          </DialogTitle>
           <DialogContent>
-            <DialogContentText id="alert-dialog-description">
+            <DialogContentText id='alert-dialog-description'>
               Are you sure you want to reject this submission ?
-                </DialogContentText>
+            </DialogContentText>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose} color="primary" autoFocus>
+            <Button onClick={handleClose} color='primary' autoFocus>
               NO
-                    </Button>
-            <Button onClick={() => handleRejectConfirm(false, props.data['_id'])} color="secondary" variant="contained" autoFocus>
+            </Button>
+            <Button
+              onClick={() => handleRejectConfirm(false, props.data["_id"])}
+              color='secondary'
+              variant='contained'
+              autoFocus>
               YES
-                    </Button>
+            </Button>
           </DialogActions>
         </Dialog>
       </CardActions>
