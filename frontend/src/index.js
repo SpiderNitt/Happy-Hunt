d413dd7a8f350a1bdf97e305f9bd3375660891c3
saisandhya01 <55455466+saisandhya01@users.noreply.github.com>
2021-02-08 16:22:44 +0530

    Admin integ (#179)
    * change home page ui
    
    * Frontend4 design (#177)
    
    * basic home page design
    
    * basic home page design
    
    * footer added
    
    * final commit changes
    
    * add verification page
    
    * change home page ui
    
    * Frontend4 design (#177)
    
    * basic home page design
    
    * basic home page design
    
    * footer added
    
    * change home page ui
    
    * Frontend4 design (#177)
    
    * basic home page design
    
    * basic home page design
    
    * footer added
    
    * final commit changes
    
    * add verification page
    
    * change home page ui
    
    * Frontend4 design (#177)
    
    * basic home page design
    
    * basic home page design
    
    * footer added
    
    * production code changes
    
    * production code changes
    
    * user notification (#185)
    
    * add minor changes (#182)
    
    * add minor changes
    
    * add more changes
    
    * feat:added share button to activity feed
    
    * add score nav
    
    * add shareIcon import
    
    * fix hint modal
    
    * activity
    
    Co-authored-by: Rishabh Taparia <rishabh.taparia5@gmail.com>
    Co-authored-by: gokul-00 <62957549+gokul-00@users.noreply.github.com>
    
    * final commit changes
    
    * add verification page
    
    * change home page ui
    
    * Add support for image upload while adding mission
    
    * Complete add mission route
    
    * Add initial values for new mission form
    
    * Update edit mission form
    
    * Update mission details component wrt to change in db
    
    * Update all mission components with latest db structure
    
    * Add feature to display missionname in the cards
    
    * start button to start clue algorithm
    
    * Start button integ and confirm messages
    
    * Convert object to formdata
    
    * string to JSON conversion with other changes
    
    * Add feature for file upload
    
    * Add edit mission with formdata
    
    * base64 to data type image base64
    
    * Edit mission with file upload
    
    * content added for verification mail
    
    * Fix: Popup appearing only once
    
    * Add popup for accept/reject submissions
    
    * Notifications for admin
    
    * Fix: start game button issue
    
    * Image submissions
    
    Co-authored-by: Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
    Co-authored-by: gokul-00 <62957549+gokul-00@users.noreply.github.com>
    Co-authored-by: gokul-00 <106119141@nitt.edu>
    Co-authored-by: Rishabh Taparia <rishabh.taparia5@gmail.com>
    Co-authored-by: unknown <mansibisen354@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Footer from "./Footer";
+// import Verify from "./components/Verify";
 // import AdminMembers from "./AdminMembers";
 // import AdminRegistration from "./AdminRegistration";
 import App from "./App";
