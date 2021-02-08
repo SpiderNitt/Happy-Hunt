495b66fdd21387d5a55e9581d698c433ac7831e6
gokul-00 <106119141@nitt.edu>
2021-02-08 11:54:21 +0530

    popup notif

--- frontend/src/utils/socketio.js
+++ frontend/src/utils/socketio.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
+import Message from "../components/Message";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000/");
 
 socket.on("connect", () => {
-  console.log(socket.id);
+  // console.log(socket.id);
 });
 
 const listener = (...args) => {
@@ -15,10 +17,24 @@ const setUnlock = () => {
 };
 
 const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-const teamID = userInfo.team;
 
-socket.on(`Notifications ${teamID}`, listener);
+if (userInfo.team !== undefined) {
+  socket.on(`Notifications ${userInfo.team}`, listener);
+}
 
 socket.on(`Notifications`, listener);
 
 socket.on(`missions`, setUnlock);
+
+function Socketio(props) {
+  const [message, setMessage] = useState("");
+  return (
+    <>
+      {message && (
+        <Message message={message} messageType='info' setMessage={setMessage} />
+      )}
+    </>
+  );
+}
+
+export default Socketio;
